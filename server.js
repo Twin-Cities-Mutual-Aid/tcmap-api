49784d3545fa7ac2837f3a37e2805651a875ce03
amaxama <maxam.anna@gmail.com>
2020-11-20 01:01:06 -0600

    Handle cross origin requests

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const airtable = require('./airtable-api');
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
 app.get("/mutual_aid_sites", function(request, response) {
   console.log("Handling mutual aid sites API request");
   airtable.getMutualAidSites(request, response);
