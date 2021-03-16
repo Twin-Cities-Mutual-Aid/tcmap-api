1f5f2d7b726dbd3bb3bfaadbd3e7984fbc8f3fd4
amaxama <maxam.anna@gmail.com>
2021-03-16 00:27:04 -0500

    Change opening to convert cst to utc

--- utils/hoursUtils.js
+++ utils/hoursUtils.js
@@ -92,8 +92,9 @@ function parseTodayHours(todayHours) {
     if (!is24Hours) {
         const openTime = todayHours.open_time_digits
         const closeTime = todayHours.close_time_digits
-        const opening = DateTime.fromObject({hour: openTime.substring(0,2), minutes: openTime.substring(2,4)})
-        console.log(opening.toLocaleString(DateTime.DATETIME_FULL))
+        const opening = DateTime.fromObject({hour: openTime.substring(0,2), minutes: openTime.substring(2,4), zone: "America/Chicago"})
+        // console.log(opening.toLocaleString(DateTime.DATETIME_FULL))
+        console.log(opening)
         const closing = DateTime.fromObject({hour: closeTime.substring(0,2), minutes: closeTime.substring(2,4), zone: "America/Chicago"}).toUTC()
         console.log(closing)
         const nowTime = DateTime.now().toUTC()
